2ee08c08a8e2dd9131e5ed029427f7888f8a780d
Xaxatix <xaxatix@hotmail.com>
2014-12-16 20:19:25 +0200

    Index

---
+++ index.php
@@ -0,0 +1,92 @@
+<?php require_once "header.html"?>
+<div id="site_content">
+	<div id="side_menu">
+		<div class="side_menu_item">
+			<a class="selected" href="#">
+				<img src="style/series_one.jpg" alt="" width="142" height="50" />
+			</a> <span class="info">Series_One</span> 
+		</div>
+		<div class="side_menu_item">
+			<a href="#">
+				<img src="style/series_two.jpg" alt="" width="142" height="50" />
+			</a> <span class="info">Series_Two</span> 
+		</div>
+		<div class="side_menu_item">
+			<a href="#">
+				<img src="style/series_three.jpg" alt="" width="142" height="50" />
+			</a> <span class="info">Series_Three</span> 
+		</div>
+		<div class="side_menu_item">
+			<a href="#">
+				<img src="style/series_four.jpg" alt="" width="142" height="50" />
+			</a> <span class="info">Series_Four</span> 
+		</div>
+		<div class="side_menu_item">
+			<a href="#">
+				<img src="style/series_five.jpg" alt="" width="142" height="50" />
+			</a> <span class="info">Series_Five</span> 
+		</div>
+	</div>
+	<div id="content">
+		<h1>Series_One Photographs (01.01.2007)<span class="sub">[hover over thumbnails to view]</span></h1>
+		<div id="gallery">
+			<div id="thumbs">
+				<a href="#nogo">
+					<img src="images/series_one/sea_1.jpg" alt="" />
+				</a>
+				<a href="#nogo">
+					<img src="images/series_one/sea_2.jpg" alt="" />
+				</a>
+				<a href="#nogo">
+					<img src="images/series_one/sea_1.jpg" alt="" />
+				</a>
+				<a href="#nogo">
+					<img src="images/series_one/sea_2.jpg" alt="" />
+				</a>
+				<a href="#nogo">
+					<img src="images/series_one/sea_1.jpg" alt="" />
+				</a>
+				<a href="#nogo">
+					<img src="images/series_one/sea_2.jpg" alt="" />
+				</a>
+				<a href="#nogo">
+					<img src="images/series_one/sea_1.jpg" alt="" />
+				</a>
+				<a href="#nogo">
+					<img src="images/series_one/sea_2.jpg" alt="" />
+				</a>
+				<a href="#nogo">
+					<img src="images/series_one/sea_1.jpg" alt="" />
+				</a>
+				<a href="#nogo">
+					<img src="images/series_one/sea_2.jpg" alt="" />
+				</a>
+				<a href="#nogo">
+					<img src="images/series_one/sea_1.jpg" alt="" />
+				</a>
+				<a href="#nogo">
+					<img src="images/series_one/sea_2.jpg" alt="" />
+				</a>
+				<a href="#nogo">
+					<img src="images/series_one/sea_1.jpg" alt="" />
+				</a>
+				<a href="#nogo">
+					<img src="images/series_one/sea_2.jpg" alt="" />
+				</a>
+				<a href="#nogo">
+					<img src="images/series_one/sea_1.jpg" alt="" />
+				</a>
+				<a href="#nogo">
+					<img src="images/series_one/sea_2.jpg" alt="" />
+				</a>
+				<a href="#nogo">
+					<img src="images/series_one/sea_1.jpg" alt="" />
+				</a>
+				<a href="#nogo">
+					<img src="images/series_one/sea_2.jpg" alt="" />
+				</a>
+			</div>
+		</div>
+	</div>
+</div>
+<?php require_once "footer.html"?>
