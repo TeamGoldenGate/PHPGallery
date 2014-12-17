3ba1b797fc98b7f758d8d9708c03750a88b2b9dd
anniy <akaneva@uni-ruse.bg>
2014-12-17 17:31:00 +0200

    upload multiply files

--- addgallery.php
+++ addgallery.php
@@ -22,12 +22,13 @@ require_once "header.php";
           </span>
       </div>
       <div class="row1">
-          <span class="formlabel">Add picture</span>
-          <span class="forminput">
-              <input type="hidden" name="MAX_FILE_SIZE" value="30000" />
-            <input type="file" name="picture" />
-          </span>
+              <span class="formlabel">Add picture</span>
+              <span id="forminput">
+                  <input type="hidden" name="MAX_FILE_SIZE" value="30000" />
+                  <input type="file" name="picture[]" multiple="multiple"/>
+              </span>
       </div>
+
       <div class="row1">
           <span class="formlabel"></span>
           <span class="forminput">
@@ -42,12 +43,18 @@ require_once "header.php";
          $galleryName = htmlentities($_POST['galleryname']);
          $category = htmlentities($_POST['category']);
          $uploaddir = './images/';
-         $uploadfile = $uploaddir . basename($_FILES['picture']['name']);
-         if (move_uploaded_file($_FILES['picture']['tmp_name'], $uploadfile)) {
-             echo "File is valid, and was successfully uploaded.\n";
-         } else {
-             echo "Possible file upload attack!\n";
-         }
+        for($i=0; $i<count($_FILES['picture']['name']); $i++) {
+            $tmpFilePath = $_FILES['picture']['tmp_name'][$i];
+            if ($tmpFilePath != "") {
+                $uploadfile = $uploaddir . basename($_FILES['picture']['name'][$i]);
+                if (move_uploaded_file($tmpFilePath, $uploadfile)) {
+                    echo "File is valid, and was successfully uploaded.\n";
+                } else {
+                    echo "Possible file upload attack!\n";
+                }
+            }
+        }
+
 		 $sql = "INSERT INTO `phpteamwork`.`albums` ( `name`, `rating`, `category`) VALUES ('$galleryName', '0', '$category')";
 
 	     if (mysqli_query($conn, $sql)) {
